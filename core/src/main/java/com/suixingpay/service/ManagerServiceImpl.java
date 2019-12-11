94513a5a5d90038a0e0761194ee837ba4c6f9c6a
jalr4ever <routerhex@qq.com>
2019-12-11 19:38:17 +0800

    悲伤的线程池宣告革命失败，回退到没使用线程池的版本

--- core/src/main/java/com/suixingpay/service/ManagerServiceImpl.java
+++ core/src/main/java/com/suixingpay/service/ManagerServiceImpl.java
@@ -7,28 +7,19 @@ package com.suixingpay.service;
 
 import com.suixingpay.mapper.ManagerMapper;
 import com.suixingpay.pojo.Manager;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.suixingpay.service.ManagerService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 public class ManagerServiceImpl implements ManagerService {
     @Resource
     private ManagerMapper managerMapper;
 
-    @Autowired
-    private ExecutorService threadPool;
-
 
     @Override
-    public Manager searchManagerById(int id) {
-        AtomicReference<Manager> manager = new AtomicReference<>();
-        threadPool.execute(() -> {
-            manager.set(managerMapper.selectUserById(id));
-        });
-        return manager.get();
+    public Manager searchManagerById (int id) {
+        return managerMapper.selectUserById(id);
     }
 }
